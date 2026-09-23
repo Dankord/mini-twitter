e284744d7b01e73fc4ca1e32cef9e4daad2ad217
Dankord <ganzonjohnkervin@email.com>
2026-09-23 17:34:23 +0800

    feat: add the homepage with layout component

---
+++ chirp/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <x-slot:title>
+        Homepage
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+                    <p class="mt-4 text-base-content/80">A brand new application made from Laravel</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
